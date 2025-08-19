a6a6d7d005794f3aff04be9cb5803bd212038506
Meliodas <alakhprateek321@gmail.com>
2025-08-18 22:24:20 -0400

    Add hostctl formula v0.1.0

--- hostctl.rb
+++ hostctl.rb
@@ -2,8 +2,8 @@ class Hostctl < Formula
   include Language::Python::Virtualenv
 
   desc "Hosts file manager CLI"
-  homepage "https://github.com/PrateekAlakh/hosts_manager"
-  url "https://github.com/PrateekAlakh/hosts_manager/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/PrateekAlakh/hostctl"
+  url "https://github.com/PrateekAlakh/hostctl/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
